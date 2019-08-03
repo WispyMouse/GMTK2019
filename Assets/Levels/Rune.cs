49c3f1ef56213fb1fbdd6f3e0d1d6349fc2df32c
Wispy Mouse <WispyMouse@gmail.com>
2019-08-03 15:30:06 -0700

    Glowing Runes, Larger Text
    Runes glow in the tutorial faintly, to teach you how to play.

--- Assets/Levels/Rune.cs
+++ Assets/Levels/Rune.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class Rune : MonoBehaviour
 {
     public SpriteRenderer RuneSpriteRenderer;
+    float ColorPongTime { get; set; } = .9f;
+    public Color ColorOne;
+    public Color ColorTwo;
+    float CurColorTime { get; set; }
+
+    private void Update()
+    {
+        CurColorTime += Time.deltaTime;
+        RuneSpriteRenderer.color = Color.Lerp(ColorOne, ColorTwo, Mathf.PingPong(CurColorTime, ColorPongTime) / ColorPongTime);
+    }
 }
